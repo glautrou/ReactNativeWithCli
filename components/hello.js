f0c884b7dd8a66139848a3f2dc0231ff5c9f8a7c
glautrou <g.lautrou@outlook.com>
2019-06-14 23:52:27 +0200

    Add webview native plugin

--- components/hello.js
+++ components/hello.js
@@ -1,13 +1,21 @@
 import React, { Component, useState } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 const Hello = ({ name }) => {
   const [count, setCount] = useState(0);
 
   return (
-    <Text>
-      Hello {name}, {count}
-    </Text>
+    <>
+      <Text>
+        Hello {name}, {count}
+      </Text>
+      <WebView
+        originWhitelist={["*"]}
+        source={{ html: "<h1>Hello world</h1>" }}
+        style={{ marginTop: 20, width: 300, height: 100 }}
+      />
+    </>
   );
 };
 
